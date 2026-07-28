540b2e4de4ff51df1b277bfbd5d579ee3fc3f5c3
Matthew Papesh <55628349+Matthew-Papesh@users.noreply.github.com>
2026-07-28 16:36:44 -0500

    deploy v3

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/matthew-papesh.github.io/",
+  base: "/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
